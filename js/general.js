d91dc13e833f0865b23329cffa2d1dc03621cb13
Alpesh Baraiya <59405951+Alpesh97@users.noreply.github.com>
2021-11-25 22:52:36 +0530

    Add files via upload

---
+++ js/general.js
@@ -0,0 +1,15 @@
+
+jQuery(document).ready(function() {	
+    // mobile-menu
+    jQuery('.site-header .hamburger-icon').click(function(){
+        jQuery('body').toggleClass('open-menu');
+    });
+});
+
+
+jQuery(window).resize(function() {
+    if (jQuery(window).innerWidth() > 767) {
+        jQuery('body').removeClass('open-menu');
+    }
+});
+
